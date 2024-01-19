74866723a59b4bac6a82f9547c79f34e47738343
deugarteromero <33359642+deugarteromero@users.noreply.github.com>
2024-01-19 07:00:01 -0800

    Multiples of 3 or 5

---
+++ multiplesOf3Or5.js
@@ -0,0 +1,14 @@
+function multiplesOfThreeOrFive(){
+    let limit = 1000;
+    let sum = 0;
+    for(let i = 0; i <= limit; i++){
+        if(i % 3 === 0 || i % 5 === 0){
+            sum += i;
+        };
+    };
+
+    return sum;
+};
+
+//TEST(S)
+console.log(multiplesOfThreeOrFive()); //Return 234168
